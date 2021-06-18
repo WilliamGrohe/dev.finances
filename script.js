40cb0262917b49a44e4708d82e698b256e399136
William Grohe <william.grohe@gmail.com>
2021-06-18 01:37:58 -0300

    Corrigindo bugs de valores

--- script.js
+++ script.js
@@ -115,8 +115,9 @@ const DOM = {
 const Utils = {
     formatAmount(value){
         value = Number(value) * 100
+
         
-        return value
+        return Math.round(value)
     },
 
     formatDate(date){
